4f3b271a00cfd3734bd4e5fd3f44400a1804f8ed
40205468 <gcleland02@qub.ac.uk>
2021-11-05 22:39:56 +0000

    Refactor camera detection

--- camera/camera_detection.py
+++ camera/camera_detection.py
@@ -5,6 +5,14 @@ import cv2
 import imutils
 import time
 
+# global variables
+LABELS = open('../yolo/coco.names').read().strip().split("\n")
+net = cv2.dnn.readNetFromDarknet('../yolo/yolov3.cfg', '../yolo/yolov3.weights')
+input_file = "../video/virat.mp4"
+output_file = "../video/output.mp4"
+frame_counter = 0
+cap = cv2.VideoCapture(input_file)
+
 
 # TODO: Display footage in real time
 # TODO: Use threads for better performance
@@ -17,47 +25,33 @@ def check(a, b):
         return False
 
 
-def setup():
-    global net, ln, LABELS
-    LABELS = open('../yolo/coco.names').read().strip().split("\n")
-    net = cv2.dnn.readNetFromDarknet('../yolo/yolov3.cfg', '../yolo/yolov3.weights')
+def getOutputLayerNames():
     ln = net.getLayerNames()
-    ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+    return [ln[i - 1] for i in net.getUnconnectedOutLayers()]
 
 
-def image_process(image):
-    global processedImg
-    (H, W) = (None, None)
-    frame = image.copy()
-    if W is None or H is None:
-        (H, W) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    starttime = time.time()
-    layerOutputs = net.forward(ln)
-    stoptime = time.time()
-    print("Video is Getting Processed at {:.4f} seconds per frame".format((stoptime - starttime)))
+def detectPerson(layers, w, h):
     confidences = []
     outline = []
 
-    print(layerOutputs)
-    exit(1)
-    for output in layerOutputs:
+    for output in layers:
         for detection in output:
             scores = detection[5:]
             maxi_class = np.argmax(scores)
             confidence = scores[maxi_class]
             if LABELS[maxi_class] == "person":
                 if confidence > 0.5:
-                    box = detection[0:4] * np.array([W, H, W, H])
+                    box = detection[0:4] * np.array([w, h, w, h])
                     (centerX, centerY, width, height) = box.astype("int")
                     x = int(centerX - (width / 2))
                     y = int(centerY - (height / 2))
                     outline.append([x, y, int(width), int(height)])
                     confidences.append(float(confidence))
 
-    box_line = cv2.dnn.NMSBoxes(outline, confidences, 0.5, 0.3)
+    return confidences, outline
 
+
+def drawBox(box_line, outline, frame):
     if len(box_line) > 0:
         flat_box = box_line.flatten()
         pairs = []
@@ -77,25 +71,38 @@ def image_process(image):
                     pairs.append([center[i], center[j]])
                     status[i] = True
                     status[j] = True
-        index = 0
 
+        index = 0
         for i in flat_box:
             (x, y) = (outline[i][0], outline[i][1])
             (w, h) = (outline[i][2], outline[i][3])
-            if status[index] == True:
+            if status[index]:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 150), 2)
-            elif status[index] == False:
+            elif not status[index]:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             index += 1
         for h in pairs:
             cv2.line(frame, tuple(h[0]), tuple(h[1]), (0, 0, 255), 2)
+    return frame
+
+
+def image_process(image):
+    global processedImg
+    (H, W) = (None, None)
+    frame = image.copy()
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layer_outputs = net.forward(getOutputLayerNames())
+
+    confidences, outline = detectPerson(layer_outputs, W, H)
+    # perform non-maxima supression
+    box_line = cv2.dnn.NMSBoxes(outline, confidences, 0.5, 0.3)
+
+    frame = drawBox(box_line, outline, frame)
     processedImg = frame.copy()
 
-
-frameno = 0
-filename = "../video/test.mp4"
-opname = "../video/output.mp4"
-cap = cv2.VideoCapture(filename)
 
 if __name__ == '__main__':
     while True:
@@ -106,10 +113,10 @@ if __name__ == '__main__':
         current_img = frame.copy()
         current_img = imutils.resize(current_img, width=480)
         video = current_img.shape
-        frameno += 1
+        frame_counter += 1
 
-        #if frameno % 2 == 0 or frameno == 1:
-        setup()
+        # if frame_counter % 2 == 0 or frame_counter == 1:
+        getOutputLayerNames()
         image_process(current_img)
         Frame = processedImg
 
