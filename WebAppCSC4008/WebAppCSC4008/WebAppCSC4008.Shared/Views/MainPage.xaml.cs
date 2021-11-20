db769d29a374c8009ba442ab3a041b5483de1302
Reuben Brown <rbrown54@qub.ac.uk>
2021-11-20 20:42:53 +0000

    Remove dummy alerts

--- WebAppCSC4008/WebAppCSC4008/WebAppCSC4008.Shared/Views/MainPage.xaml.cs
+++ WebAppCSC4008/WebAppCSC4008/WebAppCSC4008.Shared/Views/MainPage.xaml.cs
@@ -26,61 +26,10 @@ namespace WebAppCSC4008
     {
         public MainPage()
         {
-            Alert Alert1 = new Alert
-            {
-                ID = 523,
-                University = "Queen's University Belfast",
-                Room = "CSB 02/027",
-                Module = "CSC4008",
-                Lecturer = "Dr. Barry McCollum",
-                GroupSize = 2,
-                DateTime = new DateTime(2021, 11, 10, 10, 43, 12),
-                Duration = new TimeSpan(0, 34, 17),
-            };
-            Alerts.Add(Alert1);
-            Alerts.Add(Alert2);
-            Alerts.Add(Alert3);
-
             this.InitializeComponent();
 
             AlertView.ItemsSource = Alerts;
         }
-
-        public Alert Alert1 = new Alert
-        {
-            ID = 523,
-            University = "Queen's University Belfast",
-            Room = "CSB 02/027",
-            Module = "CSC4008",
-            Lecturer = "Dr. Barry McCollum",
-            GroupSize = 2,
-            DateTime = new DateTime(2021, 11, 10, 10, 43, 12),
-            Duration = new TimeSpan(0, 34, 17),
-        };
-
-        public Alert Alert2 = new Alert
-        {
-            ID = 527,
-            University = "Queen's University Belfast",
-            Room = "ASH 09/005",
-            Module = "CSC4005",
-            Lecturer = "Dr. Blesson Varghese",
-            GroupSize = 12,
-            DateTime = new DateTime(2021, 11, 10, 16, 01, 2),
-            Duration = new TimeSpan(0, 5, 5),
-        };
-
-        public Alert Alert3 = new Alert
-        {
-            ID = 463,
-            University = "Queen's University Belfast",
-            Room = "DKB 0G/115",
-            Module = "CSC4008",
-            Lecturer = "Dr. Barry McCollum",
-            GroupSize = 3,
-            DateTime = new DateTime(2021, 10, 9, 12, 00, 23),
-            Duration = new TimeSpan(0, 16, 9),
-        };
 
         public ObservableCollection<Alert> Alerts = new ObservableCollection<Alert>();
 
