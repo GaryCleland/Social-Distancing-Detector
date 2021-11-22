3007c285340ad64c4ca52c981dd9c9369d2f4098
40205468 <gcleland02@qub.ac.uk>
2021-11-22 13:15:38 +0000

    Added location to camera availability page

--- WebAppCSC4008/WebAppCSC4008/WebAppCSC4008.Shared/Models/CameraAvailability.cs
+++ WebAppCSC4008/WebAppCSC4008/WebAppCSC4008.Shared/Models/CameraAvailability.cs
@@ -10,8 +10,10 @@ namespace WebAppCSC4008.Models
         public long ID { get; set; }
         public string Name { get; set; }
         public string Status { get; set; }
+        public string Room { get; set; }
+        public string University { get; set; }
 
-        public CameraAvailability(long id, string name, string status)
+        public CameraAvailability(long id, string name, string status, string room, string university)
         {
             this.ID = id;
             this.Name = name;
@@ -22,9 +24,9 @@ namespace WebAppCSC4008.Models
         {
             return new List<CameraAvailability>(new CameraAvailability[3]
             {
-                new CameraAvailability(1, "Camera1", "Online"),
-                new CameraAvailability(2, "Camera2", "Online"),
-                new CameraAvailability(3, "Camera3", "Offline")
+                new CameraAvailability(1, "Camera1", "Online", "CSB 02/27", "Queen's University Belfast"),
+                new CameraAvailability(2, "Camera2", "Online", "CSB 01/03", "Queen's University Belfast"),
+                new CameraAvailability(3, "Camera3", "Offline", "DKB 0G/118", "Queen's University Belfast")
             });
         }
     }
