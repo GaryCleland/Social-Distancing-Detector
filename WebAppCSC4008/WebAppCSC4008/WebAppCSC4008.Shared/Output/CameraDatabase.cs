cf900803e8cf45323e3c893885c36b6805bc4a02
40205468 <gcleland02@qub.ac.uk>
2021-11-23 20:25:07 +0000

    Created camera stream DB from web app

---
+++ WebAppCSC4008/WebAppCSC4008/WebAppCSC4008.Shared/Output/CameraDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SQLite;
+
+namespace WebAppCSC4008.Output
+{
+    public class CameraDatabase
+    {
+#if __WASM__
+        static SQLiteConnection Database;
+
+        public CameraDatabase(string video)
+        {
+            var DatabasePath = Path.Combine("C:\\sqlite", "CameraStreams.db");
+            Database = new SQLiteConnection(DatabasePath);
+            var exists = File.Exists(DatabasePath);
+            if (!exists)
+            {
+                Database.CreateTable<streams>();
+            }
+            var strm = new streams() { stream = video };
+            Database.Insert(strm);
+            Database.Commit();
+            Database.Close();
+        }
+#endif
+    }
+
+    public class streams
+    {
+        [PrimaryKey]
+        public string stream { get; set; }
+    }
+}
