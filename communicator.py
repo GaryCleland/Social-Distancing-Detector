f5a38afa530ea5d9c40a964f56e64c94f7932fdb
Reuben Brown <rbrown54@qub.ac.uk>
2021-11-29 19:22:11 +0000

    Add code to send data to databases in proper locations

--- communicator.py
+++ communicator.py
@@ -3,8 +3,9 @@ import sqlite3
 
 
 class Communicator(object):
-    def __init__(self):
-        self.conn = sqlite3.connect('Alert.db', check_same_thread=False)
+    def __init__(self, f):
+        self.first = f
+        self.conn = sqlite3.connect(self.first, check_same_thread=False)
         self.cursor = self.conn.cursor()
 
     def __del__(self):
