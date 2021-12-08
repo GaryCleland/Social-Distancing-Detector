0da1635dcc8bc2c719b56e7713b900a0ed2dc811
Reuben Brown <rbrown54@qub.ac.uk>
2021-12-08 14:04:34 +0000

    Round duration to 2 decimal places

---
+++ WebAppCSC4008/WebAppCSC4008/WebAppCSC4008.Shared/Converters/TwoDecimalPointConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace WebAppCSC4008.Converters
+{
+    public class TwoDecimalPointConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double duration;
+            Double.TryParse(value.ToString(), out duration);
+            duration = Math.Round(duration, 2);
+            return duration;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
